2aa43c3b492dc78ea07ba1ce53039572eeccf545
Diego Winter <diego@diegowinter.dev>
2022-06-16 16:04:16 -0300

    feat(app): plot with yellow background for metrics with reasonable value

--- app/components/MetricPlot.tsx
+++ app/components/MetricPlot.tsx
@@ -9,18 +9,61 @@ const Plot = dynamic(() => import('react-plotly.js'), {
 
 
 const MetricPlot = ({ y_all, y_selected, is_upper, labels, name, title, width }) => {
-
   const getMedian = (arr: number[]): number => {
     const mid = Math.floor(arr.length / 2),
-      nums = [...arr].sort((a, b) => a - b);
+      nums = [...arr].sort((a, b) => a - b)
 
-    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
   };
+
+  const getFirstQuartile = (arr: number[]): number => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b)
+
+    const subArr = arr.length % 2 !== 0
+      ? nums.slice(0, mid + 1)
+      : nums.slice(0, mid)
+    const subArrMid = Math.floor(subArr.length / 2)
+
+    return subArr.length % 2 !== 0 ? subArr[subArrMid] : (subArr[subArrMid - 1] + subArr[subArrMid]) / 2
+  }
+
+  const getThirdQuartile = (arr: number[]): number => {
+    const mid = Math.floor(arr.length / 2),
+      nums = [...arr].sort((a, b) => a - b)
+
+    const subArr = nums.slice(mid, arr.length),
+      subArrMid = Math.floor(subArr.length / 2)
+
+    return subArr.length % 2 !== 0 ? subArr[subArrMid] : (subArr[subArrMid - 1] + subArr[subArrMid]) / 2
+  }
 
   const isOk = (y_selected > getMedian(y_all) ? true : false) == is_upper;
 
+  const generateColor = (): string => {
+    if (isOk) {
+      return '#c4ffcc' // verde
+    } else {
+      if (is_upper) {
+        if (y_selected >= getFirstQuartile(y_all)) {
+          return '#fceec2' // laranja
+        } else {
+          return '#fad6d6' // vermelha
+        }
+      } else {
+        if (y_selected <= getThirdQuartile(y_all)) {
+          return '#fceec2' // laranja
+        } else {
+          return '#fad6d6' // vermelha
+        }
+      }
+    }
+  }
+
+  const plotColor = generateColor()
+
   return (
-    <div className={styles.box} style={{ width: `${(width - 10)}px`, height: `${510}px`, backgroundColor: isOk ? '#c4ffcc' : '#fad6d6' }} >
+    <div className={styles.box} style={{ width: `${(width - 10)}px`, height: `${510}px`, backgroundColor: plotColor }} >
       <Plot
         data={[
           {
@@ -32,6 +75,7 @@ const MetricPlot = ({ y_all, y_selected, is_upper, labels, name, title, width })
             boxpoints: 'all',
             jitter: 0.3,
             boxmean: true,
+            quartilemethod: 'inclusive'
           },
           {
             y: [y_selected],
@@ -52,8 +96,8 @@ const MetricPlot = ({ y_all, y_selected, is_upper, labels, name, title, width })
             family: 'Lato, sans-serif',
             color: '#111111'
           },
-          plot_bgcolor: isOk ? '#c4ffcc' : '#fad6d6',
-          paper_bgcolor: isOk ? '#c4ffcc' : '#fad6d6',
+          plot_bgcolor: plotColor,
+          paper_bgcolor: plotColor,
           yaxis: {
             type: "log",
             autorange: true,
@@ -62,7 +106,7 @@ const MetricPlot = ({ y_all, y_selected, is_upper, labels, name, title, width })
           }
         }}
       />
-    </div>
+    </div >
   )
 }
 
