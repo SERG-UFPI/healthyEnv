3bf3a6b31787d0658eed041c10a2f4a1d304ba03
Diego Winter <diego@diegowinter.dev>
2022-08-13 10:27:08 -0300

    feat(app): new index page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,8 +2,13 @@ import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
+  const image = '/assets/landing.png'
+
   return (
     <>
       <Head>
@@ -11,19 +16,31 @@ export default function Home() {
       </Head>
       <Header selectedIndex={0} />
       <div className={styles.container}>
-        <div className={styles.presentation}>
-          <span className={styles.title}>
-            Análise de repositórios de software com facilidade
-          </span>
-          <span className={styles.subtitle}>
-            O healthyEnv provê <i>datasets</i> com centenas de repositórios e métricas
-            para a realização de análises que visam mostrar a saúde de um projeto de software
-            diante de projetos similares.
-          </span>
-          <div className={styles.button}>
-            <Link href='/datasets'>
-              <a>Explorar datasets</a>
-            </Link>
+        <div className={styles.firstSection}>
+          <div className={styles.imageContainer}>
+            <Image src={image} layout='fill' objectFit='contain' alt='An example of analysis.' className={styles.image} quality='100' />
+          </div>
+          <div className={styles.presentation}>
+            <span className={styles.title}>
+              Easy software repository analysis
+            </span>
+            <span className={styles.subtitle}>
+              HealthyEnv is a free tool capable of analyzing a software
+              repository based on hundreds of other repositories in our dataset.
+            </span>
+            <div className={styles.linksList}>
+              <div className={styles.button}>
+                <Link href='/datasets'>
+                  <a>Get started</a>
+                </Link>
+              </div>
+              <div className={styles.simpleButton}>
+                <Link href='/docs'>
+                  <a>See the docs</a>
+                </Link>
+                <FontAwesomeIcon icon={faChevronRight} style={{ paddingLeft: 10 }} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
